c10bc0f99d8c60bcf0da07d6a87dc5f8c5fc384e
fede1608 <fedecasares1993@yahoo.com.ar>
2014-07-08 23:39:54 -0300

    Server SSL multicliente utilizando multiples Threads

---
+++ L2j-Security-System-Server/src/L2jSecServer.java
@@ -0,0 +1,56 @@
+	import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+	public class L2jSecServer {
+	    public
+	            static
+	    void
+	            main(String[] arstring) {
+	        try {
+	            SSLServerSocketFactory sslserversocketfactory =
+	                    (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+	            SSLServerSocket sslserversocket =
+	                    (SSLServerSocket) sslserversocketfactory.createServerSocket(39999);
+	            
+	            while (true) {
+	                SSLSocket sslsocket = (SSLSocket) sslserversocket.accept();
+	                new Thread(new ServerThread(sslsocket)).start();
+	            }
+	            
+
+	        } catch (Exception exception) {
+	            exception.printStackTrace();
+	        }
+	    }
+	}
+	class ServerThread implements Runnable {
+		SSLSocket client = null;
+        public ServerThread(SSLSocket c) {
+            this.client = c;
+        }
+        public void run() {
+            try {
+                System.out.println("Connected to client : "+client.getInetAddress().getHostName());
+                InputStream inputstream = client.getInputStream();
+	            InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
+	            BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
+
+	            String string = null;
+	            while ((string = bufferedreader.readLine()) != null) {
+	                System.out.println(string);
+	                System.out.flush();
+	            }
+                client.close();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        }
+	      
+
+
